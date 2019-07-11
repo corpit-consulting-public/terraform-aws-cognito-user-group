561402b5750c29aecbc839f6350aa7e479452361
Circular-test <nmartinez@corpit-consulting.com>
2019-07-11 17:33:42 -0300

    New module cognito group

---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "name" {
+  type        = "string"
+  description = "The name of the user group"
+}
+
+variable "user_pool_id" {
+  type        = "string"
+  description = "The user pool ID"
+}
+
+variable "description" {
+  type        = "string"
+  description = "The description of the user group"
+  default     = ""
+}
+
+variable "precedence" {
+  type        = "string"
+  description = "The precedence of the user group"
+  default     = ""
+}
+
+variable "role_arn" {
+  type        = "string"
+  description = "The ARN of the IAM role to be associated with the user group"
+  default     = ""
+}
